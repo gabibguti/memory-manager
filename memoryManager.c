64f15f7ba180a92f1b23fb0abf3130aac6e897e3
gabibguti <gabi_guti_1@hotmail.com>
2017-12-06 12:39:42 -0200

    Process with dummy acess list and page table
    Page Table in SHM with diferent keys depending on process number
    Memory manager creates process and page table
    Process receives in arguments an acess list and prints it

--- memoryManager.c
+++ memoryManager.c
@@ -14,35 +14,89 @@
 
 #include "errorControl.h"	// Error Control Definitions	
 
+void pageFault (int signal);												// SIGUSR1 - Page Fault
+void startProcess (char* acessList);											// Start a process with an acess list to execute
+void createPageTable (unsigned int** pageTable, int* shmArea_pageTable, int user_process);				// Create shared memory area and attach refence to page table
+void destroyPageTable (unsigned int* pageTable, int shmArea_pageTable);							// Deattach reference to page table
+void initializeUserProcess (unsigned int** pageTable, int* shmArea_pageTable, char* acessList, int user_process);	// Create a page table for process and start process
+void endUserProcess (unsigned int* pageTable, int shmArea_pageTable);							// Destroy process page table
+void chooseKey(key_t* key_pageTable, int processNumber);								// Generates a key to create shared memory segment
+
 /* Main */
 int main(int argc, char * argv[])
 {
 	// Declarations                                                                                                       
-	int pid_memManager, pid_userProcess;
-	char * user_args[1] = { NULL } ;
+	int pid_memManager;
+	int shmArea_pageTable;
+	unsigned int* pageTable;
+	char* acessList = "acessList1.txt";
+
+	signal(SIGUSR1, pageFault); // Define SIGUSR1
 
 	pid_memManager = getpid();
 	
+	// Declarations
+	printf("Memory Manager start\n");
+
+	initializeUserProcess (&pageTable, &shmArea_pageTable, acessList, 0);
+
+	waitpid(-1, NULL, 0); // Wait for all childs (user processes) to end
+
+	endUserProcess (pageTable, shmArea_pageTable);
+
+	printf("Memory Manager end\n");
+
+	return 0;
+}
+
+/* Signal Handlers */
+
+void pageFault (int signal)
+{
+	pid_t pid_user; // User process pid that asked for page fault handle
+
+	pid_user = waitpid(-1, NULL, WNOHANG);
+
+	if(pid_user >= 0) // Process had page fault issue
+	{
+		printf("Signal %d received! Page fault on process %d!\n", signal, pid_user);
+		fflush(stdout);
+		// If phisical addres is on cache
+		// sleep(1);
+		// Else phisical addres is on HD, make swap
+		// sleep(2);
+	}
+	else if (pid_user == -1) // Something went wrong
+	{
+		printf("ERROR: PID ERROR\n");
+		fflush(stdout);
+		exit(1);
+	}
+}
+
+/* Auxiliar Functions */
+
+void startProcess (char* acessList)	// Start new process with an acess list
+{
+	int execError;
+	char * user_args[2];
+	pid_t pid_userProcess;
+
+	user_args[0] = (char*) malloc (strlen(acessList)*sizeof(char));
+	strcpy(user_args[0], acessList);
+	user_args[1] = NULL;
+
 	pid_userProcess = fork(); // Create a user process
 
 	if (pid_userProcess > 0) // Memory Manager
 	{
-		// Declarations
-		printf("\n***Memory Manager start***\n");
-
-		waitpid(pid_userProcess, NULL, 0); // Wait for user process to end
-
-		printf("\n***Memory Manager end***\n");
-
-		_exit(1); // Leave
+		// Memory manager process
 	}
 	else
 	{	
-		if (pid_userProcess == 0)	// User Process
+		if (pid_userProcess == 0) // User Process
 		{
 			pid_userProcess = getpid();
-
-			printf("\n***P1 start***\n\n");
 
 			execve( "./userProcess", user_args, NULL); // Execute user process
 			perror("execve failed");
@@ -54,6 +108,74 @@ int main(int argc, char * argv[])
 			_exit(1); // Leave
 	  	}
 	}
-
-	return 0;
 }
+
+void createPageTable (unsigned int** pageTable, int* shmArea_pageTable, int user_process)
+{
+	int errorControl;
+	key_t key_pageTable;
+	int shmSize_pageTable = (1)*sizeof(unsigned int);
+
+	// pow(2, 16)
+
+	printf("Creating page table!\n");
+
+	chooseKey(&key_pageTable, user_process);
+
+	// Page Table - Shared Memory - GET
+	*shmArea_pageTable = shmget( /* key */ key_pageTable, /* size */ shmSize_pageTable, /* flags */ IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR); 
+	failVerification(*shmArea_pageTable, shm_get);
+
+	// Page Table - Shared Memory - ATTACH
+	*pageTable = (unsigned int*) shmat( /* shared memory identifier */ *shmArea_pageTable, /* shared memory adress */ NULL, /* flags */ 0); 
+	if(*pageTable == (unsigned int*) -1) { errorControl = -1; }
+	failVerification(errorControl , shm_at); 
+}
+
+void destroyPageTable (unsigned int* pageTable, int shmArea_pageTable)
+{
+	int errorControl;
+	printf("Destroying page table!\n");
+
+	errorControl = shmdt( /* adress */ pageTable); // Page Table - Shared Memory - DEATTACH
+	failVerification(errorControl, shm_dt);
+
+	// Page Table - Shared Memory - REMOVE
+	errorControl = shmctl( /* shared memory identifier */ shmArea_pageTable, /* command */ IPC_RMID, /* pointer to data structure */ NULL);
+	failVerification(errorControl, shm_ctl);
+}
+
+void initializeUserProcess (unsigned int** pageTable, int* shmArea_pageTable, char* acessList, int user_process)
+{
+		createPageTable(pageTable, shmArea_pageTable, user_process);
+		startProcess(acessList);	
+}
+
+void endUserProcess (unsigned int* pageTable, int shmArea_pageTable)
+{
+	destroyPageTable (pageTable, shmArea_pageTable);
+}
+
+void chooseKey(key_t* key_pageTable, int processNumber)
+{
+	*key_pageTable = 8180 + processNumber;
+	printf("Page Table of process %d is associated with key %d\n", processNumber, *key_pageTable);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- userProcess.c
+++ userProcess.c
@@ -16,11 +16,173 @@
 
 #include "errorControl.h"	// Error Control Definitions	
 
+void openFile (FILE** file, char* filename);						// Opens a file by a file name
+void closeFile (FILE* file);								// Closes a file
+void readFile (FILE* file);								// Read a file and print lines
+void printFileContent (char* file_name);						// Opens a file, reads it and then close the file
+void trans(pid_t pid_process, unsigned int index, unsigned int offset, char action);	// Translate virtual adress to phisical adress
+unsigned int searchIndex (unsigned int index);						// Search index in page table and return page frame
+void getProcessKey(key_t* key_pageTable, int processNumber);
+void referencePageTable (unsigned int** pageTable, int user_process);
+void unreferencePageTable (unsigned int* pageTable);
+
 /* Main */
 int main(int argc, char *argv[])
 {
+	char* acessList;
+	unsigned int* pageTable;
+
+	acessList = (char*) malloc (strlen(argv[0])*sizeof(char));
+	strcpy(acessList, argv[0]);
+
 	printf("\n***User Process start***\n");
+
+	referencePageTable (&pageTable, 0);
+
+	printf("Process acess list: %s\n", acessList);
+	
+	printFileContent (acessList);
+
+	unreferencePageTable (pageTable);
+
 	printf("\n***User Process end***\n");
 
 	return 0;
 }
+
+/* Auxiliar Functions */
+
+void openFile (FILE** file, char* filename)
+{
+	*file = fopen(filename, "r");
+	if(*file == NULL)
+	{
+		printf("Cannot open file \n");
+		exit(0);
+	}
+}
+
+void closeFile (FILE* file)
+{
+	fclose(file);
+}
+
+void readFile (FILE* file)
+{
+	char rw;
+	unsigned int adress, index, offset;
+	pid_t pid_process;
+
+	pid_process = getpid();
+
+	// Read from acess list of process from file
+	while(fscanf(file, "%x %c ", &adress, &rw) != -1)
+	{
+		index = adress >> 16;
+		offset = (adress << 16) >> 16;
+		trans(pid_process, index, offset, rw); // Adress translation
+	}
+}
+
+void printFileContent (char* file_name)
+{
+	FILE* file;
+	char file_path[500];
+
+	strcpy(file_path, "./");
+	strcat(file_path, file_name);
+
+	openFile (&file, file_path);
+	printf("File Content:\n\n");
+	readFile (file);
+	printf("\nEnd of file\n");
+	closeFile (file);
+}
+
+void trans(pid_t pid_process, unsigned int index, unsigned int offset, char action) // Translate a virtual adress to a phisical adress
+{
+	unsigned int pageFrame;
+	pid_t pid_parent;
+
+	pid_parent = getppid(); // Memory manager pid
+
+	// Search if virtual adress has a phisical adress in page table
+	pageFrame = searchIndex (index);
+
+	if(pageFrame != 0) // Found page frame
+	{
+		printf("%d\t%x%x\t%c\n", pid_process, pageFrame, offset, action);
+		return;
+	}
+		
+	// If doesent have page frame, then send signal to memory manager
+	kill(pid_parent, SIGUSR1); // Warn memory manager that phisical adress is not allocated in phisical memory
+}
+
+unsigned int searchIndex (unsigned int index) // Searches for index in page table and returns the correspondent page frame number
+{
+	unsigned int pageFrame = 0; // Zero is reserved number for invalid page frames
+
+	if(index != 0) // Page table has index and associated page frame
+	{
+		pageFrame = 1;
+	}
+	
+	return pageFrame;
+}
+
+void getProcessKey(key_t* key_pageTable, int processNumber)
+{
+	*key_pageTable = 8180 + processNumber;
+	printf("Got Page Table key %d of process %d\n", *key_pageTable, processNumber);
+}
+
+void referencePageTable (unsigned int** pageTable, int user_process)
+{
+	int errorControl;
+	int shmArea_pageTable;
+	key_t key_pageTable;
+	int shmSize_pageTable = (1)*sizeof(unsigned int);
+
+	// pow(2, 16)
+
+	printf("Referencing page table!\n");
+
+	getProcessKey(&key_pageTable, user_process);
+
+	// Program - Shared Memory - GET
+	shmArea_pageTable = shmget( /* key */ key_pageTable, /* size */ shmSize_pageTable, /* flags */ IPC_CREAT | S_IRUSR | S_IWUSR);
+	failVerification(shmArea_pageTable, shm_get);
+ 	
+
+	// Page Table - Shared Memory - ATTACH
+	*pageTable = (unsigned int*) shmat( /* shared memory identifier */ shmArea_pageTable, /* shared memory adress */ NULL, /* flags */ 0); 
+	if(*pageTable == (unsigned int*) -1) { errorControl = -1; }
+	failVerification(errorControl , shm_at);
+}
+
+void unreferencePageTable (unsigned int* pageTable)
+{
+	int errorControl;
+	printf("Unreferencing page table!\n");
+
+	errorControl = shmdt( /* adress */ pageTable); // Page Table - Shared Memory - DEATTACH
+	failVerification(errorControl, shm_dt);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
